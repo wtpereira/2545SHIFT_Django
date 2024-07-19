d87127457febb2e4608e57e9431bfba416259f3d
Wellington <wtpereira@gmail.com>
2024-07-19 19:34:37 -0300

    Adicionar o model 'Autor'.

--- gestao/models.py
+++ gestao/models.py
@@ -2,6 +2,11 @@ from typing import Any
 from django.db import models
 
 # Create your models here.
+class Autor(models.Model):
+    nome = models.CharField(max_length=256)
+    email = models.EmailField()
+
 class Livro(models.Model):
     titulo = models.CharField(max_length=256)
     paginas = models.IntegerField()
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
